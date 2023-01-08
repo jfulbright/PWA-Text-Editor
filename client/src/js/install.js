e8900b50b5b6e61c63bd1bc9842e92d29cb7d02c
JeremyF <jfulbright@gmail.com>
2023-01-08 11:40:25 -0700

    Resolved bug to retrieve record from indexDB

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,55 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
+
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  console.log("👍", "beforeinstallprompt fired -> ", event);
+  // Stash the event so it can be triggered later.
+  window.deferredPrompt = event;
+  butInstall.classList.toggle("hidden", false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+
+butInstall.addEventListener("click", async () => {
+  console.log("click fired -> ", window.deferredPrompt);
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    console.log("promptEvent is undefined -> ", promptEvent);
+    return;
+  }
+
+  // Show the install prompt.
+  console.log("promptEvent value -> ", promptEvent);
+
+  // prompt() can only be called once.
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+  // Hide the install button.
+  butInstall.classList.toggle("hidden", true);
+
+  // butInstall.addEventListener("click", async () => {
+  //   console.log("👍", "butInstall-clicked test");
+  //   const promptEvent = window.deferredPrompt;
+  //   if (!promptEvent) {
+  //     console.log("No promptEvent");
+  //     return;
+  //   }
+  //   // Show the install prompt.
+  //   promptEvent.prompt();
+
+  //   // Reset the deferred prompt variable, since
+  //   // prompt() can only be called once.
+  //   window.deferredPrompt = null;
+  //   // Hide the install button.
+  //   butInstall.classList.toggle("hidden", true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  window.deferredPrompt = null;
+});
